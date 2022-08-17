b9b919634f4034262c79e8f8added735b6a59e13
KayZ <109565451+kekekeys@users.noreply.github.com>
2022-08-17 15:58:18 +1200

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const serverless = require("serverless-http")
+// const { collection, ObjectId } = require("../config/database")
+
+const app = express()
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    res.send("User List")
+    // collection.find().toArray((err, result) => {
+    //     if (err) throw err;
+    //     res.json(result)
+    // })
+})
+
+
+// router.post("/", (req, res) =>{
+//     res.send("create new user")
+// })
+
+// router 
+//     .router("/:id")
+//     .get((req, res) => {
+//         res.send (`get a user with ID: ${req.params.id}`)
+//     })
+//     .put((req, res) => {
+//         res.send (`Update a user with ID: ${req.params.id}`)
+//     })
+//     .delete((req, res) => {
+//         res.send (`delete a user with ID: ${req.params.id}`)
+//     })
+
+app.use("/.netlify/functions/api", router)
+module.exports.handler = serverless(app)
